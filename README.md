044f4393e928b5ee5b1b45d6d2d692300f964083
Devin Spikowski <vegeta897@gmail.com>
2021-03-04 06:30:17 -0500

    Port rewrite from vegeta897/snow-stamp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Snow-Stamp
+# ❄️ Snow-Stamp
 
 I couldn't find a page anywhere that let you just paste in a Discord snowflake to get the timestamp.
 
@@ -6,6 +6,22 @@ So I made one.
 
 ❄️ https://pixelatomy.com/snow-stamp/
 
+## Contribute
+
+Pull requests and issues are welcome!
+
+## Develop
+
+`npm install && npm run dev`
+
+### Epoch
+
+You can use a custom epoch instead of [Discord's](https://discord.com/developers/docs/reference#snowflakes) by setting environment variable `SNOWFLAKE_EPOCH` to the desired number. You can create a `.env` file in root to set this variable.
+
+## Deploy
+
+`npm run build && npm start`
+
 ---
 
-*Made with create-react-app*
+_Made with svelte_
---
+++ src/Credits.svelte
@@ -0,0 +1,37 @@
+<script>
+	import discord from './img/discord-logo.svg'
+	import github from './img/github-logo.png'
+</script>
+
+<div class="credits">
+	<p>
+		<a class="github" href="https://github.com/GuilhermeWerner/SnowStamp"
+			><img
+				height="24px"
+				src={github}
+				alt="GitHub"
+				style="position: relative; margin-right: 2px; top: 5px; margin-top: -5px;"
+			/><span style="text-decoration: none">&nbsp;</span>GitHub</a
+		>
+	</p>
+</div>
+
+<style>
+	.credits {
+		color: #777;
+		text-align: center;
+		font-size: 1em;
+		margin: 1em auto 0;
+	}
+
+	.credits code {
+		font-family: 'Fira Code', monospace;
+		font-size: 0.9em;
+		padding: 0.2em 0.3em;
+		background: #f4f4f4;
+	}
+
+	.credits span {
+		display: inline-block;
+	}
+</style>
